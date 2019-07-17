51caed5f1514f2cab9edbdcfd223a4ebee7c8a35
Silvana Ayala <silvana.ayala@nrel.gov>
2019-07-17 10:32:08 -0600

    appendtoscene and CustomObject for debugging added

--- bifacial_radiance/__init__.py
+++ bifacial_radiance/__init__.py
@@ -1,6 +1,2 @@
-from __future__ import absolute_import 
-from bifacial_radiance.main import AnalysisObj, GroundObj, MetObj, RadianceObj, SceneObj
-from bifacial_radiance.readepw import readepw
-from bifacial_radiance import load
-from bifacial_radiance import modelchain
-from bifacial_radiance.gui import gui
+from bifacial_radiance import AnalysisObj, GroundObj, MetObj, RadianceObj, SceneObj
+from readepw import readepw
